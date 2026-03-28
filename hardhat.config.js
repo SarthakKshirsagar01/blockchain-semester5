81b4fd1dee12b110b029fc0f20c6289f15232250
sarthak kshirsagar <kshirsagarsarthak9@gmail.com>
2026-03-28 13:39:26 +0530

    Day 5: verified contract on Etherscan, updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,3 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
-};
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
-};
-
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
@@ -16,9 +5,11 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_URL || process.env.ALCHEMY_SEPOLIA_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
